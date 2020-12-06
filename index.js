143f1cf6e3dc62e4d6e4ccfc469c3b929ba62067
Rafael Raupp <raupperino@gmail.com>
2020-12-06 11:35:52 -0300

    Added function to get trade execution prices on uniswap

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-require('dotenv').config()
-
-const { providers } = require('ethers')
-const Uniswap = require('./src/exchanges/uniswap')
-
-const provider = new providers.InfuraProvider('homestead', {
-    projectId: process.env.INFURA_PROJECT_ID,
-    projectSecret: process.env.INFURA_PROJECT_SECRET,
-})
-const uniswap = new Uniswap(provider);
-
 (async() => {
-    console.log('dai => weth:', (await uniswap.getPrice('dai', 'weth')).toSignificant(10))
-    console.log('dai => wbtc:', (await uniswap.getPrice('dai', 'wbtc')).toSignificant(10))
+    require('dotenv').config()
+
+    // Imports
+    const perf = require('execution-time')()
+    const { providers } = require('ethers')
+    const Uniswap = require('./src/exchanges/uniswap')
+
+    const provider = new providers.InfuraProvider('homestead', {
+        projectId: process.env.INFURA_PROJECT_ID,
+        projectSecret: process.env.INFURA_PROJECT_SECRET,
+    })
+    const uniswap = new Uniswap(provider)
+    const logPrice = (message, price) => console.log(message, price.toSignificant(10))
+
+    // Get prices
+    perf.start()
+
+    const wethDai = uniswap.getPrice('dai', 'weth')
+    const wbtcDai = uniswap.getPrice('dai', 'wbtc')
+    const btcEthTrade = uniswap.getTrade({
+        from: 'wbtc',
+        to: 'weth',
+        amount: 1,
+    })
+
+    await Promise.all([
+        wethDai,
+        wbtcDai,
+        btcEthTrade,
+    ])
+
+    console.log('Response delay:', perf.stop().preciseWords)
+
+    logPrice('weth => dai', await wethDai)
+    logPrice('wbtc => dai', await wbtcDai)
+    logPrice('Trade: 1 wbtc => weth', (await btcEthTrade).executionPrice)
 })()
--- src/exchanges/uniswap.js
+++ src/exchanges/uniswap.js
@@ -1,4 +1,5 @@
-const { ChainId, Token, Route, Fetcher } = require('@uniswap/sdk')
+const { ChainId, Token, Fetcher, Trade, Route, TokenAmount, TradeType } = require('@uniswap/sdk')
+const web3 = require('web3')
 const tokens = require('../tokens')
 
 module.exports = class Uniswap {
@@ -7,7 +8,12 @@ module.exports = class Uniswap {
     }
 
     token(ticker) {
-        return new Token(ChainId.MAINNET, tokens[ticker].address, tokens[ticker].decimals)
+        const token = tokens[ticker]
+
+        if (token)
+            return new Token(ChainId.MAINNET, token.address, token.decimals)
+
+        throw Error(`Token ${ ticker } not found`)
     }
 
     async getRoute(tickerA, tickerB) {
@@ -20,5 +26,16 @@ module.exports = class Uniswap {
 
     async getPrice(tickerA, tickerB) {
         return (await this.getRoute(tickerA, tickerB)).midPrice
+    }
+
+    async getTrade({ to, from, amount }) {
+        const toToken = this.token(to)
+        const fromToken = this.token(from)
+        const convertedAmount = web3.utils.toBN(amount).mul(web3.utils.toBN(10).pow(web3.utils.toBN(fromToken.decimals)))
+
+        const pair = await Fetcher.fetchPairData(toToken, fromToken, this.provider)
+        const route = new Route([ pair ], fromToken)
+
+        return new Trade(route, new TokenAmount(fromToken, convertedAmount), TradeType.EXACT_INPUT)
     }
 }
